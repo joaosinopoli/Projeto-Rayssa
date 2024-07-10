d85a6ad2af13eca4f80b03b2dd6158dcec17c589
joaosinopoli <joaovictorsinopoli@gmail.com>
2024-07-10 12:56:48 -0300

    initial commit

---
+++ js/scrool.js
@@ -0,0 +1,14 @@
+
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.rool-anime');
+
+elements.forEach( (element) => myObserver.observe(element) )
